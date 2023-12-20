243b583fad6a004e1b6bf2a511af102922627343
figaro-san <port_figaro@protonmail.com>
2023-12-20 15:21:25 +0900

    Update main

--- chapter_5/code/src/functions.rs
+++ chapter_5/code/src/functions.rs
@@ -1,5 +1,6 @@
 // code 5.1
-pub fn solv_frog_prob(n: usize, h: Vec<i32>) -> Vec<usize> {
+// pull-based
+pub fn solv_frog_prob_1(n: usize, h: Vec<i32>) -> Vec<usize> {
     let inf: usize = 10000000;
     let mut dp: Vec<usize> = vec![inf; n];
     dp[0] = 0;
@@ -10,7 +11,7 @@ pub fn solv_frog_prob(n: usize, h: Vec<i32>) -> Vec<usize> {
         } else {
             let cost1 = dp[i-1] + (h[i] - h[i-1]).abs() as usize; // calc cost between i-1 to i
             let cost2 = dp[i-2] + (h[i] - h[i-2]).abs() as usize; // calc cost between i-2 to i
-            dp[i] = std::cmp::min(cost1, cost2);
+            dp[i] = std::cmp::min(cost1, cost2); // choose smaller one
         }
     }
 
--- chapter_5/code/src/main.rs
+++ chapter_5/code/src/main.rs
@@ -1,7 +1,7 @@
 mod functions;
 fn main() {
-    let n = 7;
     let h: Vec<i32> = vec![2, 9, 4, 5, 1, 6, 10];
-    let ans = functions::solv_frog_prob(n, h);
+    let n = h.len();
+    let ans = functions::solv_frog_prob_1(n, h);
     println!("{:?}", ans);
 }
